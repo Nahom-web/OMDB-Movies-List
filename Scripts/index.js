813e448a0654a87e355867785aa25210ed0dcf6d
Nahom-web <52899948+Nahom-web@users.noreply.github.com>
2021-01-16 18:37:18 -0500

    Jan 16th, third update

--- Scripts/index.js
+++ Scripts/index.js
@@ -6,9 +6,7 @@ const searchedMovieBtn = $$("#searchMovie");
 const searchResult = $$("#searchResult");
 const tableValues = $$("#tableValues");
 const numberOfNominations = $$("#numberOfNominations");
-const submitMovies = $$("#submitMovies");
 const nominationsSavedMessage = $$("#nominationsSavedMessage");
-const errorMessage = $$("#errorMessage");
 
 let moviesArr = [];
 let nominatedMovies = [];
@@ -35,12 +33,33 @@ let populateSearchedMovies = async () => {
         .then(resp => {
             return resp.json();
         })
-        .then(movies => {
-            moviesArr.length = 0;
-            if (movies.Response !== "False") {
-                movies.Search.map((movie) => {
+        .then(response => {
+            if (response.Response === "False") {
+                moviesContent.innerHTML = "<h1>No Movies Found</h1>";
+            } else if (response.Response === "True") {
+                moviesArr.length = 0;
+                response.Search.map((movie) => {
                     moviesArr.push(new Movie(movie.Title, movie.Year, movie.Poster))
                 })
+                moviesContent.innerHTML = "";
+                searchResult.innerHTML = `${moviesArr.length} Results For "${searchedMovie.value}"`;
+                let count = 0;
+                for (let i in moviesArr) {
+                    moviesContent.innerHTML += `<div class="listOfMovies">
+                                            ${moviesArr[i].getPosterImage()}
+                                            <p class="movieTitleWithYear">${moviesArr[i].getMovieWithYear()}</p>
+                                            <button id="movie${count}" class="movieButtons">
+                                                Nominate
+                                                <p class="movie" hidden>${moviesArr[i]._title}</p>
+                                                <p class="year" hidden>${moviesArr[i]._year}</p>
+                                                <p class="poster" hidden>${moviesArr[i]._poster}</p>
+                                            </button>
+                                        </div>`;
+                    count++;
+                }
+                activeNominateMovies();
+            } else {
+                moviesContent.innerHTML = "<h1>No Movies Found</h1>";
             }
         })
         .catch(err => {
@@ -48,41 +67,7 @@ let populateSearchedMovies = async () => {
         })
 }
 
-
-searchedMovieBtn.addEventListener('click', (qualifiedName, value) => {
-    populateSearchedMovies();
-    if (moviesArr.length !== 0) {
-        moviesContent.innerHTML = "";
-        searchResult.innerHTML = `${moviesArr.length} Results For "${searchedMovie.value}"`;
-        let count = 0;
-        for (let i in moviesArr) {
-            moviesContent.innerHTML += `<div class="listOfMovies">
-                                            ${moviesArr[i].getPosterImage()}
-                                            <p class="movieTitleWithYear"><span>${count + 1}. </span>${moviesArr[i].getMovieWithYear()}</p>
-                                            <button id="movie${count}" class="movieButtons">
-                                                Nominate
-                                                <p class="movie" hidden>${moviesArr[i]._title}</p>
-                                                <p class="year" hidden>${moviesArr[i]._year}</p>
-                                                <p class="poster" hidden>${moviesArr[i]._poster}</p>
-                                            </button>                                                            
-                                        </div>`;
-            count++;
-        }
-        activeNominateMovies();
-    } else {
-        moviesContent.innerHTML = "<h1>No Movies Found</h1>";
-    }
-});
-
-
-let disableMovieButtons = (movie) => {
-    movie.disabled = true;
-    movie.style.backgroundColor = "darkgreen";
-}
-
-let updateMoviesSelected = () => {
-    numberOfNominations.innerHTML = nominatedMovies.length + "/5 Movies Selected";
-}
+searchedMovieBtn.addEventListener('click', populateSearchedMovies);
 
 let activeNominateMovies = () => {
     let movieButtons = document.querySelectorAll('.movieButtons');
@@ -103,12 +88,26 @@ let activeNominateMovies = () => {
             disableMovieButtons(movie);
             nominationsSavedMessage.style.display = "none";
         }
-        if (nominatedMovies.length === 5){
-            errorMessage.innerHTML = "";
+        if (nominatedMovies.length === 5) {
+            nominationsSavedMessage.style.display = "revert";
+            nominationsSavedMessage.style.width = "100%";
+            nominationsSavedMessage.style.backgroundColor = "greenyellow";
+            nominationsSavedMessage.innerHTML = `<h2 style="text-align: center">Nominations Saved!</h2>`;
+            for (let i in nominatedMovies) {
+                localStorage.setItem(`movie${i}`, `${nominatedMovies[i].Title} (${nominatedMovies[i].Year})`);
+            }
         }
     }))
 }
 
+let disableMovieButtons = (movie) => {
+    movie.disabled = true;
+    movie.style.backgroundColor = "darkgreen";
+}
+
+let updateMoviesSelected = () => {
+    numberOfNominations.innerHTML = nominatedMovies.length + "/5 Movies Selected";
+}
 
 let setNominatedList = (arr) => {
     tableValues.innerHTML = "";
@@ -119,7 +118,6 @@ let setNominatedList = (arr) => {
                                         <td>${arr[i].Year}</td>
                                         <td>
                                             <button class="deleteButtons">
-                                                Remove
                                                 <p id="movieId" hidden>${arr[i].ID}</p>
                                             </button>                                            
                                         </td>
@@ -151,21 +149,18 @@ let removeMovie = () => {
     updateMoviesSelected();
 }
 
-submitMovies.addEventListener('click', () => {
-    console.log('submit movies');
-    if (nominatedMovies.length === 5) {
-        nominationsSavedMessage.style.display = "revert";
-        nominationsSavedMessage.style.width = "100%";
-        nominationsSavedMessage.style.backgroundColor = "greenyellow";
-        nominationsSavedMessage.innerHTML = `<h2 style="text-align: center">Nominations Saved!</h2>`;
-        for (let i in nominatedMovies) {
-            localStorage.setItem(`movie${i}`, `${nominatedMovies[i].Title} (${nominatedMovies[i].Year})`);
-        }
-    }
-    else{
-        errorMessage.innerHTML = "*Please Select 5 Movies";
-    }
-})
+// submitMovies.addEventListener('click', () => {
+//     console.log('submit movies');
+//     if (nominatedMovies.length === 5) {
+//         nominationsSavedMessage.style.display = "revert";
+//         nominationsSavedMessage.style.width = "100%";
+//         nominationsSavedMessage.style.backgroundColor = "greenyellow";
+//         nominationsSavedMessage.innerHTML = `<h2 style="text-align: center">Nominations Saved!</h2>`;
+//         for (let i in nominatedMovies) {
+//             localStorage.setItem(`movie${i}`, `${nominatedMovies[i].Title} (${nominatedMovies[i].Year})`);
+//         }
+//     }
+// })
 
 
 window.addEventListener('load', () => {
